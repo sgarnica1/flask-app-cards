c0cb8f343102ae15969aacfbf73ca715bc2a8876
Sergio Garnica <sgarnica1902@gmail.com>
2022-01-04 18:44:33 -0600

    Environment variables implemented with dotenv

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+import os
+from cards.service_card import ServiceCard
+from cards.credit_card import CreditCard
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
-from cards.credit_card import CreditCard
-from cards.service_card import ServiceCard
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 
 # SESSION SETTINGS
-app.secret_key = 'mysecretkey'
+app.secret_key = os.environ.get('MYSECRETKEY')
 
 # MYSQL CONNECTION
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_DB'] = 'flask_app_cards'
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 mysql = MySQL(app)
 
 
