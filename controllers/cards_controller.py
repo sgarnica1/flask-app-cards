9460df9696f0e5f3ead9c5e5ff6b4fbd4181f1ff
Sergio Garnica <sgarnica1902@gmail.com>
2022-01-11 13:50:31 -0600

    Try except blocks added for db connection

--- controllers/cards_controller.py
+++ controllers/cards_controller.py
@@ -5,6 +5,7 @@ from cards.credit_card import CreditCard
 from cards.service_card import ServiceCard
 
 # CONTROLLER
+from controllers.index_controller import index
 from controllers.user_controller import index
 import controllers.user_controller as user_controller
 
@@ -19,11 +20,15 @@ mysql = MySQL(app)
 
 # GET index
 def index(type: str, card_number: int):
-    cursor = mysql.connection.cursor()
-    cursor.execute(
-        f'SELECT * FROM {type}_cards WHERE card_number = {card_number}')
-    data = cursor.fetchall()
-    data = data[0]
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            f'SELECT * FROM {type}_cards WHERE card_number = {card_number}')
+        data = cursor.fetchall()
+        data = data[0]
+    except:
+        flash('An error ocurred. Please try again later', 'danger')
+        return redirect(url_for('index_bp.index'))
 
     # QUERY USER
     user_id = data[1]
@@ -47,140 +52,167 @@ def index(type: str, card_number: int):
 
 # GET ADD CARD
 def add_card_get(user_id: int, type: str):
-    user = user_controller.query_user(user_id)
-    return render_template(
-        f'add-card.html',
-        user=user[0],
-        title=f"Add {type.capitalize()} Card",
-        type=type
-    )
+    try:
+        user = user_controller.query_user(user_id)
+        return render_template(
+            f'add-card.html',
+            user=user[0],
+            title=f"Add {type.capitalize()} Card",
+            type=type
+        )
+    except:
+        flash('An error ocurred. Please try again later.', 'danger')
+        return redirect(url_for('index_bp.index'))
 
 
 # POST ADD CARD
 def add_card_post(type: str):
-    if request.method == 'POST':
-        # GET FORM DATA
-        user_id = request.form['user_id']
-        loan = float(request.form['loan'])
+    try:
+        if request.method == 'POST':
+            # GET FORM DATA
+            user_id = request.form['user_id']
+            loan = float(request.form['loan'])
 
-        # QUERY USER AND GET FULLNAME
-        user = user_controller.query_user(user_id)[0]
-        fullname = f'{user[1]} {user[2]}'
+            # QUERY USER AND GET FULLNAME
+            user = user_controller.query_user(user_id)[0]
+            fullname = f'{user[1]} {user[2]}'
 
-        # CURSOR MYSQL
-        cursor = mysql.connection.cursor()
+            # CURSOR MYSQL
+            cursor = mysql.connection.cursor()
 
-        if type == "credit":
-            interest_rate = float(request.form['interest_rate'])
-            payment = float(request.form['payment'])
-            new_charges = float(request.form['new_charges'])
+            if type == "credit":
+                interest_rate = float(request.form['interest_rate'])
+                payment = float(request.form['payment'])
+                new_charges = float(request.form['new_charges'])
 
-            # CREATE CREDIT CARD INSTANCE BASED ON REQUEST INFO
-            new_card = CreditCard(fullname, user_id, interest_rate,
-                                  loan, payment, new_charges)
+                # CREATE CREDIT CARD INSTANCE BASED ON REQUEST INFO
+                new_card = CreditCard(fullname, user_id, interest_rate,
+                                      loan, payment, new_charges)
 
-            # INSERT NEW CARD INTO MYSQL TABLE
-            cursor.execute("""
-              INSERT INTO credit_cards (user_id, card_number, expiration_date, cvv, type, interest_rate, loan, payment, new_charges, new_loan)
-              VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-              """, (user_id, new_card.card_number, new_card.expiration_date, new_card.cvv, new_card.type, new_card.interest_rate, new_card.loan, new_card.payment, new_card.new_charges, new_card.new_loan))
+                # INSERT NEW CARD INTO MYSQL TABLE
+                cursor.execute("""
+                INSERT INTO credit_cards (user_id, card_number, expiration_date, cvv, type, interest_rate, loan, payment, new_charges, new_loan)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, (user_id, new_card.card_number, new_card.expiration_date, new_card.cvv, new_card.type, new_card.interest_rate, new_card.loan, new_card.payment, new_card.new_charges, new_card.new_loan))
 
-        elif type == "service":
-            new_card = ServiceCard(fullname, user_id, loan)
+            elif type == "service":
+                new_card = ServiceCard(fullname, user_id, loan)
 
-            # INSERT NEW SERVICE CARD INTO MYSQL TABLE
-            cursor.execute("""
-              INSERT INTO service_cards (user_id, card_number, expiration_date, cvv, type, loan, payment)
-              VALUES (%s, %s, %s, %s, %s, %s, %s)
-              """, (user_id, new_card.card_number, new_card.expiration_date, new_card.cvv, new_card.type, new_card.loan, new_card.payment))
+                # INSERT NEW SERVICE CARD INTO MYSQL TABLE
+                cursor.execute("""
+                INSERT INTO service_cards (user_id, card_number, expiration_date, cvv, type, loan, payment)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """, (user_id, new_card.card_number, new_card.expiration_date, new_card.cvv, new_card.type, new_card.loan, new_card.payment))
 
-        # COMMIT MYSQL CODE
-        mysql.connection.commit()
+            # COMMIT MYSQL CODE
+            mysql.connection.commit()
 
-        return redirect(url_for('.index', type=type, card_number=new_card.card_number))
+            return redirect(url_for('.index', type=type, card_number=new_card.card_number))
+    except:
+        flash(
+            'There was an error creating the card. Please try again later.', 'danger')
+        return redirect(url_for('user_bp.index', id=user_id))
 
 
 # DELETE CARD
 def delete_card(type: str, id: int):
-    cursor = mysql.connection.cursor()
-    # GET USER ID
-    cursor.execute(f'SELECT user_id from {type}_cards WHERE card_id={id}')
-    user_id = cursor.fetchall()[0]
+    try:
+        cursor = mysql.connection.cursor()
+        # GET USER ID
+        cursor.execute(f'SELECT user_id from {type}_cards WHERE card_id={id}')
+        user_id = cursor.fetchall()[0]
 
-    # DELETE CARD
-    cursor.execute(f'DELETE FROM {type}_cards WHERE card_id = {id}')
-    mysql.connection.commit()
+        # DELETE CARD
+        cursor.execute(f'DELETE FROM {type}_cards WHERE card_id = {id}')
+        mysql.connection.commit()
 
-    # REDIRECT TO USER PAGE
-    return redirect(url_for('user_bp.index', id=user_id))
+        # REDIRECT TO USER PAGE
+        return redirect(url_for('user_bp.index', id=user_id))
+    except:
+        flash(
+            'There was an error deleting the card. Please try again later.', 'danger')
+        return redirect(url_for('index_bp.index'))
 
 
 # CREATE CARD REPORT
 def create_report(type: str, card_number: str):
-    cursor = mysql.connection.cursor()
-    cursor.execute(
-        f'SELECT * FROM {type}_cards WHERE card_number = {card_number}')
-    data = cursor.fetchall()
-    data = data[0]
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            f'SELECT * FROM {type}_cards WHERE card_number = {card_number}')
+        data = cursor.fetchall()
+        data = data[0]
 
-    # QUERY USER
-    user_id = data[1]
-    user = user_controller.query_user(user_id)[0]
-    user_dict = user_controller.create_user_dict(user)
+        # QUERY USER
+        user_id = data[1]
+        user = user_controller.query_user(user_id)[0]
+        user_dict = user_controller.create_user_dict(user)
 
-    if type == 'credit':
-        card_dict = create_credit_card_dict(data)
-        card = CreditCard(user_dict['fullname'], user_id, card_dict['interest_rate'],
-                          card_dict['loan'], card_dict['payment'], card_dict['new_charges'])
+        if type == 'credit':
+            card_dict = create_credit_card_dict(data)
+            card = CreditCard(user_dict['fullname'], user_id, card_dict['interest_rate'],
+                              card_dict['loan'], card_dict['payment'], card_dict['new_charges'])
 
-    elif type == "service":
-        card_dict = create_service_card_dict(data)
-        card = ServiceCard(user_dict["fullname"], user_id, card_dict['loan'])
+        elif type == "service":
+            card_dict = create_service_card_dict(data)
+            card = ServiceCard(
+                user_dict["fullname"], user_id, card_dict['loan'])
 
-    card.card_number = card_dict['card_number']['full']
-    card.expiration_date = card_dict["expiration_date"]["full"]
-    card.cvv = card_dict['cvv']
+        card.card_number = card_dict['card_number']['full']
+        card.expiration_date = card_dict["expiration_date"]["full"]
+        card.cvv = card_dict['cvv']
 
-    card.create_report()
+        card.create_report()
 
-    return redirect(url_for('.index', card_number=card_number, type=type))
+        flash(
+            'Card report created successfully!', 'success')
+        return redirect(url_for('.index', card_number=card_number, type=type))
+
+    except:
+        flash(
+            'Error creating card report. Please try again later.', 'danger')
+        return redirect(url_for('.index', card_number=card_number, type=type))
 
 
 # RECALCULATE LOAN
 def recalculate_loan(card_number: str):
-    if request.method == 'POST':
-        loan = float(request.form['new_loan'])
-        interest_rate = float(request.form['interest_rate'])
-        payment = float(request.form['payment'])
-        new_charges = float(request.form['new_charges'])
+    try:
+        if request.method == 'POST':
+            loan = float(request.form['new_loan'])
+            interest_rate = float(request.form['interest_rate'])
+            payment = float(request.form['payment'])
+            new_charges = float(request.form['new_charges'])
 
-    credit_card = CreditCard(
-        'Default user', 0, interest_rate, loan, payment, new_charges)
+            credit_card = CreditCard(
+                'Default user', 0, interest_rate, loan, payment, new_charges)
 
-    new_loan = credit_card.new_loan
+            new_loan = credit_card.new_loan
 
-    cursor = mysql.connection.cursor()
+            cursor = mysql.connection.cursor()
 
-    cursor.execute(f"""
-        UPDATE credit_cards
-        SET loan={loan},
-        payment={payment},
-        new_charges={new_charges},
-        new_loan={new_loan}
-        WHERE card_number={card_number}
-      """)
-    mysql.connection.commit()
+            cursor.execute(f"""
+              UPDATE credit_cards
+              SET loan={loan},
+              payment={payment},
+              new_charges={new_charges},
+              new_loan={new_loan}
+              WHERE card_number={card_number}
+            """)
+            mysql.connection.commit()
 
-    print(credit_card)
-    return redirect(url_for('.index', type='credit', card_number=card_number))
+            print(credit_card)
+            return redirect(url_for('.index', type='credit', card_number=card_number))
+    except:
+        flash(
+            'Error recalculating loan. Please try again later.', 'danger')
+        return redirect(url_for('.index', type='credit', card_number=card_number))
 
 
 # TOTAL PAYMENT
 def make_total_payment(type_card: str, card_number: str):
-    if request.method == 'POST':
-        cursor = mysql.connection.cursor()
-
-        try:
+    try:
+        if request.method == 'POST':
+            cursor = mysql.connection.cursor()
             if type_card == 'credit':
                 cursor.execute(
                     f'SELECT new_loan FROM credit_cards WHERE card_number={card_number}')
@@ -220,19 +252,18 @@ def make_total_payment(type_card: str, card_number: str):
 
                     flash('Payment made successfully!', 'success')
 
-        except:
-            flash(
-                'There was an error with the payment. Please try again later.', 'danger')
+    except:
+        flash(
+            'There was an error with the payment. Please try again later.', 'danger')
 
-        return redirect(url_for('.index', type=type_card, card_number=card_number))
+    return redirect(url_for('.index', type=type_card, card_number=card_number))
 
 
 # PARTIAL PAYMENT
 def make_partial_payment(card_number: str):
-    if request.method == 'POST':
-        cursor = mysql.connection.cursor()
-
-        try:
+    try:
+        if request.method == 'POST':
+            cursor = mysql.connection.cursor()
             cursor.execute(
                 f'SELECT payment, new_loan FROM credit_cards WHERE card_number={card_number}')
             payment, loan = cursor.fetchall()[0]
@@ -255,15 +286,11 @@ def make_partial_payment(card_number: str):
 
                 flash('Payment made successfully!', 'success')
 
-        except:
-            flash(
-                'There was an error with the payment. Please try again later.', 'danger')
+    except:
+        flash(
+            'There was an error with the payment. Please try again later.', 'danger')
 
-        return redirect(url_for('.index', type='credit', card_number=card_number))
-
-    return "Pay partial"
-
-# MIDDLEWARE
+    return redirect(url_for('.index', type='credit', card_number=card_number))
 
 
 # HELPERS
